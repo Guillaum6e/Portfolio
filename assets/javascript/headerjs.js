310e048dab607e37756d26497efdafe3b09ac4df
GUILLAUME GROS <guillaume.grosm11@gmail.com>
2023-05-19 17:09:35 +0200

    desktop header

--- assets/javascript/headerjs.js
+++ assets/javascript/headerjs.js
@@ -9,3 +9,33 @@ function headerScroll() {
         header.style.transform = "translate(0, 0vh)";
     }
 }
+
+// const flippingSpan = document.getElementsByClassName("flippingSpan");
+// let timestamp = null;
+// let lastMouseX = null;
+// let lastMouseY = null;
+
+// header.onmousemove = function mouseSpeed(e) {
+//     if (timestamp === null) {
+//         timestamp = Date.now();
+//         lastMouseX = e.clientX;
+//         lastMouseY = e.clientY;
+//     }
+
+//     let now = Date.now();
+//     let dt = now - timestamp;
+//     let dx = e.clientX - lastMouseX;
+//     let dy = e.clientY - lastMouseY;
+//     let speedX = Math.abs(dx / dt * 100);
+//     let speedY = Math.abs(dy / dt * 100);
+//     let speed = Math.round(speedX + speedY / 2);
+
+//     flippingSpan.onmouseover = function () {
+//         console.log(yousk2);
+//         flippingSpan.style.transform = "rotateX(" + speed * 360 + "deg)";
+//     };
+
+//     timestamp = now;
+//     lastMouseX = e.clientX;
+//     lastMouseY = e.clientY;
+// };
